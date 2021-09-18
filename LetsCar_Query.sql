6ecb10469bff52829e4ac7117d1ef0e0dda4648d
Beatriz de Castro Resende <83736310+beatrizresende@users.noreply.github.com>
2021-09-18 02:19:58 -0300

    Inserindo EXECS, retirando NOT NULL de procedures e colocando selects faltantes

--- LetsCar_Query.sql
+++ LetsCar_Query.sql
@@ -21,13 +21,13 @@ CREATE PROCEDURE InserirMarca
 			VALUES (@NomeMarca)
 	END
 
-EXEC InserirMarca @NomeMarca = 'Honda'
-EXEC InserirMarca @NomeMarca = 'Fiat'
-EXEC InserirMarca @NomeMarca = 'Tesla'
-EXEC InserirMarca @NomeMarca = 'BMW'
-EXEC InserirMarca @NomeMarca = 'VolksWagen'
-EXEC InserirMarca @NomeMarca = 'Ferrari'
-EXEC InserirMarca @NomeMarca = 'Vou tentar editar essa'
+--EXEC InserirMarca @NomeMarca = 'Honda'
+--EXEC InserirMarca @NomeMarca = 'Fiat'
+--EXEC InserirMarca @NomeMarca = 'Tesla'
+--EXEC InserirMarca @NomeMarca = 'BMW'
+--EXEC InserirMarca @NomeMarca = 'VolksWagen'
+--EXEC InserirMarca @NomeMarca = 'Ferrari'
+--EXEC InserirMarca @NomeMarca = 'Vou tentar editar essa'
 
 GO
 CREATE PROCEDURE EditarMarca
@@ -40,7 +40,7 @@ CREATE PROCEDURE EditarMarca
 			WHERE Id = @IdMarca
 	END
 
-EXEC EditarMarca @IdMarca = 7, @NomeMarca = 'Consegui editar!'
+--EXEC EditarMarca @IdMarca = 7, @NomeMarca = 'Consegui editar!'
 
 GO
 CREATE PROCEDURE DeletarMarca
@@ -51,7 +51,7 @@ CREATE PROCEDURE DeletarMarca
 			WHERE Id = @IdMarca
 	END
 
-EXEC DeletarMarca @IdMarca = 7
+--EXEC DeletarMarca @IdMarca = 7
 
 SELECT * FROM Marcas
 
@@ -75,10 +75,10 @@ CREATE PROCEDURE InserirModelo
 			VALUES (@NomeModelo, @IdMarca)
 	END
 
-EXEC InserirModelo @NomeModelo = 'Honda Civic', @IdMarca = 1
-EXEC InserirModelo @NomeModelo = 'Ferrari 250 GT', @IdMarca = 6
-EXEC InserirModelo @NomeModelo = 'Uno Pallo', @IdMarca = 2
-EXEC InserirModelo @NomeModelo = 'Vou tentar editar essa', @IdMarca = 3
+--EXEC InserirModelo @NomeModelo = 'Honda Civic', @IdMarca = 1
+--EXEC InserirModelo @NomeModelo = 'Ferrari 250 GT', @IdMarca = 6
+--EXEC InserirModelo @NomeModelo = 'Uno Pallo', @IdMarca = 2
+--EXEC InserirModelo @NomeModelo = 'Vou tentar editar essa', @IdMarca = 3
 
 GO
 CREATE PROCEDURE EditarModelo
@@ -93,16 +93,18 @@ CREATE PROCEDURE EditarModelo
 				WHERE Id = @IdModelo
 	END
 
-EditarModelo 
+--EXEC EditarModelo @NomeModelo = 'Modelo editado!', @IdMarca = 3,  @IdModelo = 4
 
 GO
 CREATE PROCEDURE DeletarModelo
-	@IdModelo INT NOT NULL
+	@IdModelo INT
 	AS
 	BEGIN
 		DELETE FROM Modelos
 			WHERE Id = @IdModelo
 	END
+
+--EXEC DeletarModelo @IdModelo = 4
 
 --Cores---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Cores
@@ -114,17 +116,24 @@ CREATE TABLE Cores
 
 GO
 CREATE PROCEDURE InserirCor
-	@NomeCor VARCHAR(50) NOT NULL
+	@NomeCor VARCHAR(50)
 	AS
 	BEGIN
 		INSERT INTO Cores(Nome)
 			VALUES (@NomeCor)
 	END
 
+--EXEC InserirCor @NomeCor = 'Azul'
+--EXEC InserirCor @NomeCor = 'Vermelho'
+--EXEC InserirCor @NomeCor = 'Cinza'
+--EXEC InserirCor @NomeCor = 'Branco'
+--EXEC InserirCor @NomeCor = 'Preto'
+--EXEC InserirCor @NomeCor = 'Teste de edição'
+
 GO
 CREATE PROCEDURE EditarCor
-	@NomeCor VARCHAR(50) NOT NULL,
-	@IdCor INT NOT NULL
+	@NomeCor VARCHAR(50),
+	@IdCor INT
 	AS
 	BEGIN
 		UPDATE Cores
@@ -132,14 +141,18 @@ CREATE PROCEDURE EditarCor
 				WHERE Id = @IdCor
 	END
 
+--EXEC EditarCor @NomeCor = 'Cor editada', @IdCor = 6
+
 GO
 CREATE PROCEDURE DeletarCor
-	@IdCor INT NOT NULL
+	@IdCor INT
 	AS
 	BEGIN
 		DELETE FROM Cores
 			WHERE Id = @IdCor
 	END
+
+--EXEC DeletarCor @IdCor = 6
 
 --Carros---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Carros
@@ -150,22 +163,24 @@ CREATE TABLE Carros
 	IdCor INT NOT NULL,
 	Preco DECIMAL(20,2) NOT NULL,
 	CONSTRAINT PK_Carros PRIMARY KEY(Id),
-	CONSTRAINT FK_Carros_Marcas FOREIGN KEY(IdMarca) REFERENCES Marcas(Id),
 	CONSTRAINT FK_Carros_Modelos FOREIGN KEY(IdModelo) REFERENCES Modelos(Id),
 	CONSTRAINT FK_Carros_Cores FOREIGN KEY(IdCor) REFERENCES Cores(Id)
 )
 
 GO
 CREATE PROCEDURE InserirCarro
-	@IdModelo INT NOT NULL,
-	@Ano CHAR(4) NOT NULL,
-	@IdCor INT NOT NULL,
-	@Preco DECIMAL(20,2) NOT NULL
+	@IdModelo INT,
+	@Ano CHAR(4),
+	@IdCor INT,
+	@Preco DECIMAL(20,2)
 	AS
 	BEGIN
 		INSERT INTO Carros(IdModelo, Ano, IdCor, Preco)
 			VALUES (@IdModelo, @Ano, @IdCor, @Preco)
 	END
+
+--EXEC InserirCarro @IdModelo = 2, @Ano = 2000, @IdCor = 3, @Preco = 200000000.00
+--EXEC InserirCarro @IdModelo = 3, @Ano = 2010, @IdCor = 2, @Preco = 300000.00
 
 GO
 CREATE PROCEDURE EditarCarro
@@ -173,7 +188,7 @@ CREATE PROCEDURE EditarCarro
 	@Ano CHAR(4),
 	@IdCor INT,
 	@Preco DECIMAL(20,2),
-	@IdCarro INT NOT NULL
+	@IdCarro INT
 	AS
 	BEGIN
 		UPDATE Carros
@@ -184,14 +199,18 @@ CREATE PROCEDURE EditarCarro
 				WHERE Id = @IdCarro
 	END
 
+--EXEC EditarCarro @IdModelo = 3, @Ano = 2010, @IdCor = 4, @Preco = 3500000.00, @IdCarro = 2
+
 GO
 CREATE PROCEDURE DeletarCarro
-	@IdCarro INT NOT NULL
+	@IdCarro INT
 	AS
 	BEGIN
 		DELETE FROM Carros
 			WHERE Id = @IdCarro
 	END
+
+--EXEC DeletarCarro @IdCarro = 2
 
 --Adicionais---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -204,17 +223,21 @@ CREATE TABLE Adicionais
 
 GO
 CREATE PROCEDURE InserirAdicional
-	@NomeAdicional VARCHAR(100) NOT NULL
+	@NomeAdicional VARCHAR(100) 
 	AS
 	BEGIN
 		INSERT INTO Adicionais(Nome)
 			VALUES (@NomeAdicional)
 	END
 
+--EXEC InserirAdicional @NomeAdicional = 'Ar condicionado'
+--EXEC InserirAdicional @NomeAdicional = 'Vidro blindado'
+--EXEC InserirAdicional @NomeAdicional = 'Vou editar esse'
+
 GO
 CREATE PROCEDURE EditarAdicional
-	@NomeAdicional VARCHAR(100) NOT NULL,
-	@IdAdicional INT NOT NULL
+	@NomeAdicional VARCHAR(100),
+	@IdAdicional INT
 	AS
 	BEGIN
 		UPDATE Adicionais
@@ -222,14 +245,18 @@ CREATE PROCEDURE EditarAdicional
 				WHERE Id = @IdAdicional
 	END
 
+--EXEC EditarAdicional @NomeAdicional = 'Editando', @IdAdicional = 3
+
 GO
 CREATE PROCEDURE DeletarAdicional
-	@IdAdicional INT NOT NULL
+	@IdAdicional INT
 	AS
 	BEGIN
 		DELETE FROM Adicionais
 			WHERE Id = @IdAdicional
 	END
+
+--EXEC DeletarAdicional @IdAdicional = 3
 
 CREATE TABLE AdicionaisCarros
 (
@@ -243,19 +270,21 @@ CREATE TABLE AdicionaisCarros
 
 GO
 CREATE PROCEDURE InserirAdicionalCarro
-	@IdAdicional INT NOT NULL,
-	@IdCarro INT NOT NULL
+	@IdAdicional INT,
+	@IdCarro INT
 	AS
 	BEGIN
 		INSERT INTO AdicionaisCarros(IdAdicional, IdCarro)
 			VALUES (@IdAdicional, @IdCarro)
 	END
 
+--EXEC InserirAdicionalCarro @IdAdicional = 2, @IdCarro = 1
+
 GO
 CREATE PROCEDURE EditarAdicionalCarro
 	@IdAdicional INT,
 	@IdCarro INT,
-	@IdOperacao INT NOT NULL
+	@IdOperacao INT
 	AS
 	BEGIN
 		UPDATE AdicionaisCarros
@@ -264,14 +293,18 @@ CREATE PROCEDURE EditarAdicionalCarro
 				WHERE Id = @IdOperacao
 	END
 
+--EXEC EditarAdicionalCarro @IdAdicional = 1, @IdCarro = 1, @IdOperacao = 1
+
 GO
 CREATE PROCEDURE DeletarAdicionalCarro
-	@IdOperacao INT NOT NULL
+	@IdOperacao INT
 	AS
 	BEGIN
 		DELETE FROM AdicionaisCarros
 			WHERE Id = @IdOperacao
 	END
+
+--EXEC DeletarAdicionalCarro @IdOperacao = 1
 
 --Modos de pagamento---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE ModosPagamento
@@ -283,17 +316,22 @@ CREATE TABLE ModosPagamento
 
 GO
 CREATE PROCEDURE InserirModoPagamento
-	@ModoPagamento VARCHAR(50) NOT NULL
+	@ModoPagamento VARCHAR(50)
 	AS
 	BEGIN
 		INSERT INTO ModosPagamento(Nome)
 			VALUES (@ModoPagamento)
 	END
 
+--EXEC InserirModoPagamento @ModoPagamento = 'À vista'
+--EXEC InserirModoPagamento @ModoPagamento = 'Crédito'
+--EXEC InserirModoPagamento @ModoPagamento = 'Débito'
+--EXEC InserirModoPagamento @ModoPagamento = 'Teste'
+
 GO
 CREATE PROCEDURE EditarModoPagamento
-	@ModoPagamento VARCHAR(50) NOT NULL,
-	@IdModoPagamento INT NOT NULL
+	@ModoPagamento VARCHAR(50),
+	@IdModoPagamento INT
 	AS
 	BEGIN
 		UPDATE ModosPagamento
@@ -301,14 +339,18 @@ CREATE PROCEDURE EditarModoPagamento
 				WHERE Id = @IdModoPagamento
 	END
 
+--EXEC EditarModoPagamento @ModoPagamento = 'Editando', @IdModoPagamento = 4
+
 GO
 CREATE PROCEDURE DeletarModoPagamento
-	@IdModoPagamento INT NOT NULL
+	@IdModoPagamento INT
 	AS
 	BEGIN
 		DELETE FROM ModosPagamento
 			WHERE Id = @IdModoPagamento
 	END
+
+--EXEC DeletarModoPagamento @IdModoPagamento = 4
 
 CREATE TABLE ModosPagamentoCarros
 (
@@ -322,35 +364,42 @@ CREATE TABLE ModosPagamentoCarros
 
 GO
 CREATE PROCEDURE InserirModoPagamentoCarro
-	@IdModo INT NOT NULL,
-	@IdCarro INT NOT NULL
+	@IdModo INT,
+	@IdCarro INT
 	AS
 	BEGIN
 		INSERT INTO ModosPagamentoCarros(IdModo, IdCarro)
 			VALUES (@IdModo, @IdCarro)
 	END
 
+--EXEC InserirModoPagamentoCarro @IdModo = 2, @IdCarro = 1
+--EXEC InserirModoPagamentoCarro @IdModo = 3, @IdCarro = 1
+
 GO
 CREATE PROCEDURE EditarModoPagamentoCarro
 	@IdModo INT,
 	@IdCarro INT,
-	@IdOperacao INT NOT NULL
+	@IdOperacao INT
 	AS
 	BEGIN
 		UPDATE ModosPagamentoCarros
-			SET IdModo = IsNull(@IdModo, IdModo),
-			IdCarro = IsNull(@IdCarro, IdCarro)
+			SET IdModo = @IdModo,
+			IdCarro = @IdCarro
 				WHERE Id = @IdOperacao
 	END
 
+--EXEC EditarModoPagamentoCarro @IdModo = 1, @IdCarro = 1, @IdOperacao = 3
+
 GO
 CREATE PROCEDURE DeletarModoPagamentoCarro
-	@IdOperacao INT NOT NULL
+	@IdOperacao INT
 	AS
 	BEGIN
 		DELETE FROM ModosPagamentoCarros
 			WHERE Id = @IdOperacao
 	END
+
+--EXEC DeletarModoPagamentoCarro @IdOperacao = 3
 
 --Usuarios---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Usuarios
@@ -366,16 +415,20 @@ CREATE TABLE Usuarios
 
 GO
 CREATE PROCEDURE InserirUsuario
-	@Nome VARCHAR(200) NOT NULL,
-	@CPF VARCHAR(14) NOT NULL,
-	@DataNascimento DATE NOT NULL,
-	@Email VARCHAR(256) NOT NULL,
-	@Senha VARCHAR(50) NOT NULL
+	@Nome VARCHAR(200),
+	@CPF VARCHAR(14),
+	@DataNascimento DATE,
+	@Email VARCHAR(256),
+	@Senha VARCHAR(50)
 	AS
 	BEGIN
 		INSERT INTO Usuarios(Nome, CPF, DataNascimento, Email, Senha)
 			VALUES (@Nome, @CPF, @DataNascimento, @Email, @Senha)
 	END
+
+--EXEC InserirUsuario @Nome = 'Vinicius Mussak', @CPF = '738291813', @DataNascimento = '08-10-1990', @Email = 'vini123@gmail.com', @Senha = 'vini123'
+--EXEC InserirUsuario @Nome = 'Teste', @CPF = '738291813', @DataNascimento = '08-10-1990', @Email = 'vini123@gmail.com', @Senha = 'vini123'
+
 
 GO
 CREATE PROCEDURE EditarUsuario
@@ -384,26 +437,30 @@ CREATE PROCEDURE EditarUsuario
 	@DataNascimento DATE,
 	@Email VARCHAR(256),
 	@Senha VARCHAR(50),
-	@IdUsuario INT NOT NULL
+	@IdUsuario INT
 	AS
 	BEGIN
 		UPDATE Usuarios
-			SET Nome = IsNull(@Nome, Nome),
-			CPF = IsNull(@CPF, CPF),
-			DataNascimento = IsNull(@DataNascimento, DataNascimento),
-			Email = IsNull(@Email, Email),
-			Senha = IsNull(@Senha, Senha)
+			SET Nome = @Nome,
+			CPF = @CPF,
+			DataNascimento = @DataNascimento,
+			Email = @Email,
+			Senha = @Senha
 				WHERE Id = @IdUsuario
 	END
 
+--EXEC EditarUsuario @Nome = 'Editando o teste', @CPF = '738291813', @DataNascimento = '08-10-1990', @Email = 'vini123@gmail.com', @Senha = 'vini123', @IdUsuario = 2
+
 GO
 CREATE PROCEDURE DeletarUsuario
-	@IdUsuario INT NOT NULL
+	@IdUsuario INT
 	AS
 	BEGIN
 		DELETE FROM Usuarios
 			WHERE Id = @IdUsuario
 	END
+
+--EXEC DeletarUsuario @IdUsuario = 2
 
 --Favoritados---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE FavoritosUsuario
@@ -418,19 +475,21 @@ CREATE TABLE FavoritosUsuario
 
 GO
 CREATE PROCEDURE FavoritarCarro
-	@IdUsuario INT NOT NULL,
-	@IdCarro INT NOT NULl
+	@IdUsuario INT,
+	@IdCarro INT
 	AS
 	BEGIN
 		INSERT INTO FavoritosUsuario(IdUsuario, IdCarro)
 			VALUES (@IdUsuario, @IdCarro)
 	END
 
+--EXEC FavoritarCarro @IdUsuario = 1, @IdCarro = 1
+
 GO
 CREATE PROCEDURE EditarCarroFavoritado
 	@IdUsuario INT,
 	@IdCarro INT,
-	@IdOperacao INT NOT NULL
+	@IdOperacao INT
 	AS
 	BEGIN
 		UPDATE FavoritosUsuario
@@ -439,9 +498,11 @@ CREATE PROCEDURE EditarCarroFavoritado
 				WHERE Id = @IdOperacao
 	END
 
+--EXEC EditarCarroFavoritado @IdUsuario = 1, @IdCarro = 1, @IdOperacao = 1
+
 GO
 CREATE PROCEDURE ExibirCarrosFavoritados
-	@IdUsuario INT NOT NULL
+	@IdUsuario INT
 	AS
 	BEGIN
 		SELECT m.Nome AS Modelo,
@@ -461,14 +522,18 @@ CREATE PROCEDURE ExibirCarrosFavoritados
 												WHERE fu.IdUsuario = @IdUsuario
 	END
 
+--EXEC ExibirCarrosFavoritados @IdUsuario = 1
+
 GO
 CREATE PROCEDURE DeletarCarroFavoritado
-	@IdOperacao INT NOT NULL
+	@IdOperacao INT
 	AS
 	BEGIN
 		DELETE FROM FavoritosUsuario
 			WHERE Id = @IdOperacao
 	END
+
+--EXEC DeletarCarroFavoritado @IdOperacao = 1
 
 --Formulário de contato---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -479,14 +544,15 @@ CREATE TABLE Contato
 	IdCarro INT NOT NULL,
 	Mensagem VARCHAR(MAX) NOT NULL,
 	CONSTRAINT PK_Contato PRIMARY KEY(Id),
+	CONSTRAINT FK_Contato_Usuarios FOREIGN KEY(IdUsuario) REFERENCES Usuarios(Id),
 	CONSTRAINT FK_Contato_Carros FOREIGN KEY(IdCarro) REFERENCES Carros(Id)
 )
 
 GO
 CREATE PROCEDURE CriarFormularioContato
-	@IdUsuario INT NOT NULL,
-	@IdCarro INT NOT NULL,
-	@Mensagem VARCHAR(MAX) NOT NULL
+	@IdUsuario INT,
+	@IdCarro INT,
+	@Mensagem VARCHAR(MAX)
 	AS
 	BEGIN
 		INSERT INTO Contato(IdUsuario, IdCarro, Mensagem)
@@ -498,7 +564,7 @@ CREATE PROCEDURE EditarFormularioContato
 	@IdUsuario INT,
 	@IdCarro INT,
 	@Mensagem VARCHAR(MAX),
-	@IdFormulario INT NOT NULL
+	@IdFormulario INT
 	AS
 	BEGIN
 		UPDATE Contato
@@ -510,10 +576,7 @@ CREATE PROCEDURE EditarFormularioContato
 
 GO
 CREATE PROCEDURE DeletarFormularioContato
-	@IdUsuario INT,
-	@IdCarro INT,
-	@Mensagem VARCHAR(MAX),
-	@IdFormulario INT NOT NULL
+	@IdFormulario INT
 	AS
 	BEGIN
 		DELETE FROM Contato
@@ -524,14 +587,15 @@ CREATE PROCEDURE DeletarFormularioContato
 --Buscar por atributo---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 GO
 CREATE PROCEDURE BuscarMarca
-	@Marca VARCHAR(50) NOT NULL
+	@Marca VARCHAR(50)
 	AS
 	BEGIN
 		SELECT m.Nome AS Marca,
 		mo.Nome AS Modelo,
 		c.Ano,
 		co.Nome AS Cor,
-		c.Preco
+		c.Preco,
+		mp.Nome AS ModoPagamento
 			FROM Marcas m
 				INNER JOIN Modelos mo
 					ON m.Id = mo.IdMarca
@@ -539,21 +603,26 @@ CREATE PROCEDURE BuscarMarca
 							ON mo.Id = c.IdModelo
 								INNER JOIN Cores co
 									ON co.Id = c.IdCor
-										WHERE m.Nome = @Marca
+										INNER JOIN ModosPagamentoCarros mpc
+											ON mpc.IdCarro = c.Id
+												INNER JOIN ModosPagamento mp
+													ON mpc.IdModo = mp.Id
+														WHERE m.Nome = @Marca
 	END
 
-EXEC BuscarMarca @Marca = 'Fiat'
+--EXEC BuscarMarca @Marca = 'FERRARI'
 
 GO
 CREATE PROCEDURE BuscarModelo
-	@Modelo VARCHAR(100) NOT NULL
+	@Modelo VARCHAR(100)
 	AS
 	BEGIN
 		SELECT mo.Nome AS Modelo,
 		m.Nome AS Marca,
 		c.Ano,
 		co.Nome AS Cor,
-		c.Preco
+		c.Preco,
+		mp.Nome AS ModoPagamento
 			FROM Modelos mo
 				INNER JOIN Marcas m
 					ON mo.IdMarca = m.Id
@@ -561,5 +630,120 @@ CREATE PROCEDURE BuscarModelo
 							ON mo.Id = c.IdModelo
 								INNER JOIN Cores co
 									ON co.Id = c.IdCor
-										WHERE mo.Nome = @Modelo
+										INNER JOIN ModosPagamentoCarros mpc
+											ON mpc.IdCarro = c.Id
+												INNER JOIN ModosPagamento mp
+													ON mpc.IdModo = mp.Id
+														WHERE mo.Nome = @Modelo
 	END
+
+--EXEC BuscarModelo @Modelo = 'FERRARI 250 GT'
+
+GO
+CREATE PROCEDURE BuscarAno
+	@Ano CHAR(4)
+	AS
+	BEGIN
+		SELECT c.Ano,
+		mo.Nome AS Modelo,
+		m.Nome AS Marca,
+		co.Nome AS Cor,
+		c.Preco,
+		mp.Nome AS ModoPagamento
+			FROM Carros c
+				INNER JOIN Modelos mo
+					ON mo.Id = c.IdModelo
+						INNER JOIN Marcas m
+							ON m.Id = mo.IdMarca
+								INNER JOIN Cores co
+									ON co.Id = c.IdCor
+										INNER JOIN ModosPagamentoCarros mpc
+											ON mpc.IdCarro = c.Id
+												INNER JOIN ModosPagamento mp
+													ON mpc.IdModo = mp.Id
+														WHERE c.Ano = @Ano
+	END
+
+--EXEC BuscarAno @Ano = '2000'
+
+GO
+CREATE PROCEDURE BuscarCor
+	@Cor VARCHAR(50)
+	AS
+	BEGIN
+		SELECT co.Nome AS Cor,
+		mo.Nome AS Modelo,
+		m.Nome AS Marca,
+		c.Ano,
+		c.Preco,
+		mp.Nome AS ModoPagamento
+			FROM Cores co
+				INNER JOIN Carros c
+					ON co.Id = c.IdCor
+						INNER JOIN Modelos mo
+							ON mo.Id = c.IdModelo
+								INNER JOIN Marcas m
+									ON m.Id = mo.IdMarca
+										INNER JOIN ModosPagamentoCarros mpc
+											ON mpc.IdCarro = c.Id
+												INNER JOIN ModosPagamento mp
+													ON mpc.IdModo = mp.Id
+														WHERE co.Nome = @Cor
+	END
+
+--EXEC BuscarCor @Cor = 'Cinza'
+
+GO
+CREATE PROCEDURE BuscarPreco
+	@Preco DECIMAL(20,2)
+	AS
+	BEGIN
+		SELECT c.Preco,
+		mo.Nome AS Modelo,
+		m.Nome AS Marca,
+		c.Ano,
+		co.Nome AS Cor,
+		mp.Nome AS ModoPagamento
+			FROM Carros c
+				INNER JOIN Modelos mo
+					ON mo.Id = c.IdModelo
+						INNER JOIN Marcas m
+							ON mo.IdMarca = m.Id
+								INNER JOIN Cores co
+									ON co.Id = c.IdCor
+										INNER JOIN ModosPagamentoCarros mpc
+											ON mpc.IdCarro = c.Id
+												INNER JOIN ModosPagamento mp
+													ON mpc.IdModo = mp.Id
+														WHERE c.Preco = @Preco
+	END
+
+--EXEC BuscarPreco @Preco = 200000000.00
+
+
+GO
+CREATE PROCEDURE BuscarModoPagamento
+	@ModoPagamento VARCHAR(50)
+	AS
+	BEGIN
+		SELECT mp.Nome AS ModoPagamento,
+		mo.Nome AS Modelo,
+		m.Nome AS Marca,
+		c.Ano,
+		co.Nome AS Cor,
+		c.Preco
+			FROM ModosPagamentoCarros mpc
+				INNER JOIN ModosPagamento mp
+					ON mp.Id = mpc.IdModo
+						INNER JOIN Carros c
+							ON c.Id = mpc.IdCarro
+								INNER JOIN Modelos mo
+									ON c.IdModelo = mo.Id
+										INNER JOIN Marcas m
+											ON mo.IdMarca = m.Id
+												INNER JOIN Cores co
+													ON co.Id = c.IdCor
+														WHERE mp.Nome = @ModoPagamento
+	END
+
+--EXEC BuscarPreco @Preco = 200000000.00
